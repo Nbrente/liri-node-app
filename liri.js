ef7ca6528a6dbd8437f89e96dd451f227f63ec0f
Nicolas Brent Ellerbe <nbrente@gmail.com>
2018-03-28 17:25:46 -0400

    finished build

--- liri.js
+++ liri.js
@@ -9,13 +9,14 @@ const OmdbApi = require('omdb-api-pt')
 
 // Create a new instance of the module.
 const omdb = new OmdbApi({
-  apiKey: '73bfe6c5',
-  baseUrl: 'https://omdbapi.com/'
+  apiKey: process.env.OMDB_APIKEY,
+  baseUrl: process.env.OMDB_BASE_URL
 })
 
-
-
-const spotify = new Spotify(keys.spotify);
+const spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -24,12 +25,68 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });;
 
-
 var params = {
   screen_name: 'nodejs'
 };
 
+function logCommand(data) {
+  fs.appendFile('log.txt', JSON.stringify(data, null, 2), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+}
 
+var logTemp;
+
+function getTweets(choice) {
+  console.log("opened my tweets.")
+  client.get('statuses/user_timeline', {
+    q: 'node.js'
+  }, function (error, tweets, response) {
+    logTemp = tweets;
+    logCommand(logTemp);
+    console.log("Tweets")
+    console.log("---------------");;
+    for (let i = 0; i < 20; i++) {
+      console.log("My Recent Tweet #" + (i + 1) + " of 20")
+      console.log(tweets[i].created_at);
+      console.log(tweets[i].text);
+      console.log("---------------");
+    }
+  });
+}
+
+function getSpotify(response) {
+  spotify.search({
+    type: 'track',
+    query: response.song,
+    limit: 1
+  }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else {
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name)
+      console.log("Artist(s): " + data.tracks.items[0].artists[0].name)
+      console.log("Spotify Link: " + data.tracks.items[0].external_urls.spotify)
+      logCommand(data);
+    }
+  });
+}
+
+function omdbSearch(response) {
+  var isMovie = "movie";
+  if (response.type != "A Movie?") {
+    isMovie = "series";
+  }
+  omdb.bySearch({
+      search: response.name,
+      type: isMovie,
+      year: response.yearStart,
+      page: 1
+    }).then(res => console.log(JSON.stringify(res, null, 2)))
+    .catch(err => console.error(err))
+}
 
 inquirer
   .prompt([
@@ -43,20 +100,7 @@ inquirer
   ]).then(function (inquirerResponse) {
     switch (inquirerResponse.choice) {
       case `my-tweets`:
-        console.log("opened my tweets.")
-        client.get('statuses/user_timeline', {
-          q: 'node.js'
-        }, function (error, tweets, response) {
-
-          console.log("Tweets")
-          console.log("---------------");;
-          for (let i = 0; i < 20; i++) {
-            console.log("My Recent Tweet #" + (i + 1) + " of 20")
-            console.log(tweets[i].created_at);
-            console.log(tweets[i].text);
-            console.log("---------------");
-          }
-        });
+        getTweets()
         break;
 
 
@@ -70,20 +114,7 @@ inquirer
               name: "song"
             }
           ]).then(function (inquirerResponse) {
-            spotify.search({
-              type: 'track',
-              query: inquirerResponse.song,
-              limit: 1
-            }, function (err, data) {
-              if (err) {
-                return console.log('Error occurred: ' + err);
-              } else {
-                console.log("Song: " + data.tracks.items[0].name);
-                console.log("Album: " + data.tracks.items[0].album.name)
-                console.log("Artist(s): " + data.tracks.items[0].artists[0].name)
-                console.log("Spotify Link: " + data.tracks.items[0].external_urls.spotify)
-              }
-            });
+            getSpotify(inquirerResponse)
           })
         break;
 
@@ -108,17 +139,7 @@ inquirer
               name: "yearStart"
             }
           ]).then(function (inquirerResponse) {
-            var isMovie = "movie";
-            if (inquirerResponse.type != "A Movie?") {
-              isMovie = "series";
-            }
-            omdb.bySearch({
-                search: inquirerResponse.name,
-                type: isMovie,
-                year: inquirerResponse.yearStart,
-                page: 1
-              }).then(res => console.log(JSON.stringify(res, null, 2)))
-              .catch(err => console.error(err))
+            omdbSearch(inquirerResponse);
           })
         break;
 
@@ -126,7 +147,31 @@ inquirer
       case `do-what-it-says`:
         fs.readFile('random.txt', "UTF-8", (err, data) => {
           if (err) throw err;
-          data.split(',')
+          var command = data.split(',');
+          if (command[0] == "my-tweets") {
+            getTweets()
+          } else if (command[0] == "spotify-this-song") {
+            var song = command[1];
+            var response = {
+              song
+            };
+            getSpotify(response)
+
+          } else if (command[0] == "movie-this") {
+            // in order to mimic my inquirer response, the random.txt format must be: command,movie/series,name,yearStart
+            var objectMimic = {
+              type: command[1],
+              name: command[2],
+              yearStart: command[3]
+            };
+
+            omdbSearch(objectMimic);
+
+          } else if (command[0] == "do-what-it-says") {
+            console.log("This command loop isn't supported by 'do-what-it-says'.")
+          } else {
+            console.log("No command detected in random.txt")
+          }
         });
 
         break;
